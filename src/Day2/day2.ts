abec5f0b8b87f8e511dcb22b9091c45a612cc25b
Batuhan Şanlı <baskanbatuhan@hotmail.com>
2022-12-02 16:22:20 +0300

    add day 2 - part 2

--- src/Day2/day2.ts
+++ src/Day2/day2.ts
@@ -64,3 +64,38 @@ fs.readFile('./input.txt', (err, rawData) => {
     console.log("Player 1 score:", player1);  // 11270
     console.log("Player 2 (YOU) score:", player2);  // 14375
 });
+
+// Part 2
+const turnMove = {
+    X: 'lose',
+    Y: 'draw',
+    Z: 'win',
+}
+
+const getHandShape = (opponentShape: string, round: string) => {
+    if (round === 'draw') {
+        return opponentShape
+    } else if (round === 'win') {
+        return winsTable[winsTable[opponentShape]];
+    } else {
+        return winsTable[opponentShape];
+    }
+}
+
+fs.readFile('./input.txt', (err, rawData) => {
+    const data = rawData.toString().split('\n');
+    let player1 = 0;
+    let player2 = 0;
+
+    data.forEach(line => {
+        if (line) {
+            const [p1, p2] = line.split(' ');
+            const p2Shape = getHandShape(turnRps[p1], turnMove[p2])
+            const points = getPoints(turnRps[p1], p2Shape);
+            player1 += points.player1;
+            player2 += points.player2;
+        }
+    });
+    console.log("Player 1 score:", player1);  // 15581
+    console.log("Player 2 (YOU) score:", player2);  // 10274
+});
