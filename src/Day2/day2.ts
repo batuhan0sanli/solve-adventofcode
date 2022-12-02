84cfa9a836aee2e9530ed5f8208407634d49d5c6
Batuhan Şanlı <baskanbatuhan@hotmail.com>
2022-12-02 16:21:27 +0300

    add day 2 - part 1

---
+++ src/Day2/day2.ts
@@ -0,0 +1,66 @@
+import * as fs from 'fs';
+
+const rpsPoint = {
+    rock: 1,
+    paper: 2,
+    scissors: 3
+};
+
+const turnRps = {
+    A: 'rock',
+    B: 'paper',
+    C: 'scissors',
+    X: 'rock',
+    Y: 'paper',
+    Z: 'scissors',
+};
+
+const winsTable = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper'
+};
+
+const getWinner = (player1: string, player2: string) => {
+    if (player1 === player2) {
+        return 'draw';
+    } else if (winsTable[player1] === player2) {
+        return 'player1';
+    } else {
+        return 'player2';
+    }
+}
+
+const getPoints = (player1: string, player2: string) => {
+    const winner = getWinner(player1, player2);
+    let result;
+    if (winner === 'draw') {
+        result = [3, 3];
+    } else if (winner === 'player1') {
+        result = [6, 0];
+    } else {
+        result = [0, 6];
+    }
+    return {
+        player1: result[0] + rpsPoint[player1],
+        player2: result[1] + rpsPoint[player2]
+    }
+}
+
+// Part 1
+fs.readFile('./input.txt', (err, rawData) => {
+    const data = rawData.toString().split('\n');
+    let player1 = 0;
+    let player2 = 0;
+
+    data.forEach(line => {
+        if (line) {
+            const [p1, p2] = line.split(' ');
+            const points = getPoints(turnRps[p1], turnRps[p2]);
+            player1 += points.player1;
+            player2 += points.player2;
+        }
+    });
+    console.log("Player 1 score:", player1);  // 11270
+    console.log("Player 2 (YOU) score:", player2);  // 14375
+});
