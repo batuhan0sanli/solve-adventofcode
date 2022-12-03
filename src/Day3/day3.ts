c105d60db48898ca4802b68cab1d22d94448eb0c
Batuhan Şanlı <batuhansanli@gmail.com>
2022-12-03 16:24:30 +0300

    add day 3 - part 2

--- src/Day3/day3.ts
+++ src/Day3/day3.ts
@@ -24,3 +24,38 @@ fs.readFile('./input.txt', (err, rawData) => {
     });
     console.log("Total priority: ", totalPriority);  // 8085
 });
+
+// Part 2
+const findIntersections = (compartments: Array<string>): Set<string> => {
+    return compartments.reduce((acc, compartment) => {
+        return new Set(compartment.split('').filter(x => acc.has(x)));
+    }, new Set(compartments[0]));
+}
+
+const groupArray = (arr: Array<any>, size: number): Array<any> => {
+    let groupedArray = [];
+    arr.reduce((acc, value) => {
+        if (acc.length === size - 1) {
+            acc.push(value)
+            groupedArray.push(acc);
+            return [];
+        } else {
+            acc.push(value);
+            return acc;
+        }
+    }, []);
+    return groupedArray;
+};
+
+fs.readFile('./input.txt', (err, rawData) => {
+    const data = rawData.toString().split('\n');
+    let totalPriority = 0;
+    const groupedData = groupArray(data, 3);
+
+    groupedData.forEach(rucksacks => {
+        const intersection = findIntersections(rucksacks);
+        const priority = Array.from(intersection).reduce((acc, letter) => acc + letterPriority(letter), 0);
+        totalPriority += priority;
+    });
+    console.log("Sum of the priorities of those item types: ", totalPriority);  // 2515
+});
