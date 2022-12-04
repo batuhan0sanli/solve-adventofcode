96c92e423a02d0d29f33770bf0210767e713f38a
Batuhan Şanlı <batuhansanli@gmail.com>
2022-12-04 13:39:22 +0300

    add day 4 - part 1

---
+++ src/Day4/day4.ts
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+
+const isFullyContain = (elf1: string, elf2: string): boolean => {
+    const elf1Sections = elf1.split('-').map((section => Number(section)))
+    const elf2Sections = elf2.split('-').map((section => Number(section)))
+
+    if (elf1Sections[0] <= elf2Sections[0] && elf1Sections[1] >= elf2Sections[1])
+        return true;
+    else
+        return elf1Sections[0] >= elf2Sections[0] && elf1Sections[1] <= elf2Sections[1];
+};
+
+// Part 1
+fs.readFile('./input.txt', (err, rawData) => {
+    const data = rawData.toString().split('\n');
+    let totalOfFullyContain = 0;
+    data.forEach(pairs => {
+        if (!pairs) {
+            return;
+        }
+        const elf1 = pairs.split(',')[0]
+        const elf2 = pairs.split(',')[1]
+        if (isFullyContain(elf1, elf2)) {
+            totalOfFullyContain += 1
+        }
+    })
+    console.log("Assignment pairs does one range fully contain the other:", totalOfFullyContain)  // 413
+});
+
