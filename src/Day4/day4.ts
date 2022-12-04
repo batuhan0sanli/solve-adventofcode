6ad1b89f78dc55a8241fad8663d1ef3ef443b032
Batuhan Şanlı <batuhansanli@gmail.com>
2022-12-04 13:48:49 +0300

    add day 4 - part 2

--- src/Day4/day4.ts
+++ src/Day4/day4.ts
@@ -27,3 +27,29 @@ fs.readFile('./input.txt', (err, rawData) => {
     console.log("Assignment pairs does one range fully contain the other:", totalOfFullyContain)  // 413
 });
 
+// Part 2
+const isOverlap = (elf1: string, elf2: string): boolean => {
+    const elf1Sections = elf1.split('-').map((section => Number(section)))
+    const elf2Sections = elf2.split('-').map((section => Number(section)))
+
+    if (elf1Sections[1] >= elf2Sections[0] && elf1Sections[0] <= elf2Sections[1])
+        return true;
+    else
+        return elf1Sections[1] <= elf2Sections[0] && elf1Sections[0] >= elf2Sections[1]
+};
+
+fs.readFile('./input.txt', (err, rawData) => {
+    const data = rawData.toString().split('\n');
+    let totalOfOverlap = 0;
+    data.forEach(pairs => {
+        if (!pairs) {
+            return;
+        }
+        const elf1 = pairs.split(',')[0]
+        const elf2 = pairs.split(',')[1]
+        if (isOverlap(elf1, elf2)) {
+            totalOfOverlap += 1
+        }
+    })
+    console.log("Assignment pairs do the ranges overlap:", totalOfOverlap)  // 413
+});
