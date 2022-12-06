d5218574fb16f9e55e5dc5b93a9cf32937092759
Batuhan Şanlı <baskanbatuhan@hotmail.com>
2022-12-06 17:07:01 +0300

    add day 6 - part 1

---
+++ src/Day6/day6.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+
+const isOnlyDistinctCharacters = (message: string): boolean => {
+    const setMessage = new Set(message);
+    return setMessage.size === message.length;
+}
+
+// Part 1
+fs.readFile('./input.txt', (err, rawData) => {
+    const data = rawData.toString().split('\n');
+    const characters = data[0].split('');
+    const sizeOfDifferentCharacters = 4;
+
+    let findIndex;
+    for (let i=0; i<characters.length; i++) {
+        const testMessage = characters.slice(i, i+sizeOfDifferentCharacters).join('');
+        if (isOnlyDistinctCharacters(testMessage)) {
+            findIndex = i + sizeOfDifferentCharacters;
+            break;
+        }
+    }
+    console.log("Part 1 Result:", findIndex)  // 1647
+});
