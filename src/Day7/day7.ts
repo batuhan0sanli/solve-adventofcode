8c7d55dfcd76c7b137b6205ff5cbc150b019499c
Batuhan Şanlı <baskanbatuhan@hotmail.com>
2022-12-07 13:55:12 +0300

    add day 7 - part 2

--- src/Day7/day7.ts
+++ src/Day7/day7.ts
@@ -86,3 +86,67 @@ fs.readFile('./input.txt', (err, rawData) => {
     const size = sumFolderSizeWithMaxSize(fileSystem, 100000)
     console.log("Part 1 Result:", size) // 919137
 });
+
+// Part 2
+const getMinDeleteFolderSize = (folder: object, minDeleteSize: number): number => {
+    let minSize = Infinity;
+    Object.keys(folder).forEach(key => {
+        if (typeof folder[key] === 'object') {
+            const folder_size = folderSize(folder[key])
+            if (folder_size >= minDeleteSize && minSize > folder_size) {
+                minSize = folder_size
+            }
+            const minSizeInFolder = getMinDeleteFolderSize(folder[key], minDeleteSize)
+            if (minSizeInFolder < minSize) {
+                minSize = minSizeInFolder
+            }
+        }
+    })
+    return minSize
+}
+
+fs.readFile('./input.txt', (err, rawData) => {
+    const data = rawData.toString().split('\n');
+    const fileSystem = {'/': {}};
+    let current_directory = [];
+    data.forEach(line => {
+        if (!line) {
+            return;
+        }
+        if (line.startsWith('$')) {
+            const command = line.split(' ')[1]
+            const param = line.split(' ')[2]
+
+            if (command === 'cd') {
+                if (param === '..') {
+                    current_directory.pop()
+                } else if (param === '/') {
+                    current_directory = ['/']
+                } else {
+                    current_directory.push(param)
+                }
+
+            } else if (command === 'ls') {
+                return;
+            }
+
+            // line is result
+        } else {
+            if (line.startsWith('dir')) {
+                const folder_name = line.split(' ')[1]
+                mkdir(fileSystem, current_directory, folder_name)
+            } else {
+                const file_size = Number(line.split(' ')[0])
+                const file_name = line.split(' ')[1]
+                touch(fileSystem, current_directory, file_name, file_size)
+            }
+        }
+    });
+    const totalSize = folderSize(fileSystem)
+    const maxCapacity = 70000000
+    const updateSize = 30000000
+    const minDeleteSize = updateSize + totalSize - maxCapacity
+
+    const size = getMinDeleteFolderSize(fileSystem, minDeleteSize)
+    console.log("Part 2 Result:", size) // 2877389
+});
